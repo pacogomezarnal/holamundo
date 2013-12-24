7f1063b3dc2654e2e48b50b5288b1d3a15528cd1
Paco Gomez <fjgomez@florida-uni.es>
2013-12-24 07:20:39 +0100

    Cambiamos la impresion

--- HolaMundo/src/Presentacion.java
+++ HolaMundo/src/Presentacion.java
@@ -3,7 +3,7 @@ public class Presentacion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola proyecto!!!");
+		System.out.println("Buenos días!!!");
 	}
 
 }
